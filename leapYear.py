2ff031d42b855bd043caf78ed5016488690928c7
shashwatxx <shashwatmishra2111@gmail.com>
2021-05-03 01:13:52 +0530

    leap 2

--- leapYear.py
+++ leapYear.py
@@ -2,13 +2,24 @@
 year = int(input("enter the year\n"))
 
 
-if(year % 4 == 0):
-    if(year % 100 != 0):
-        if(year % 400 != 0):
-            print("It's a Leap Year!")
+# if(year % 4 == 0):
+#     if(year % 100 != 0):
+#         if(year % 400 != 0):
+#             print("It's a Leap Year!")
+#         else:
+#             print("It's not a Leap Year")
+#     else:
+#         print("It's not a Leap Year")
+# else:
+#     print("Its not a Leap Year")
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
         else:
-            print("It's not a Leap Year")
+            print("Not Leap Year")
     else:
-        print("It's not a Leap Year")
+        print("Leap Year")
 else:
-    print("Its not a Leap Year")
+    print("Not Leap Year")
