159b22c6470dbace750a9fb9dd4d15e8cc3b8873
shashwatxx <shashwatmishra2111@gmail.com>
2021-05-03 00:01:20 +0530

    bmi done

--- bmi.py
+++ bmi.py
@@ -1,15 +1,15 @@
 height = float(input("Enter your height in m\n"))
 weight = float(input("Enter your weight in kg\n"))
 
-bmi = round((weight/(height**2)))
+bmi = round((weight/height ** 2))
 
 if bmi < 18.5:
-    print("Your BMI is {bmi}.You are UnderWeight")
+    print(f"Your BMI is {bmi}.You are UnderWeight")
 elif bmi < 25:
-    print("Your BMI is {bmi}.You are UnderWeight")
-elif bmi < 25:
-    print("Your BMI is {bmi}.You are UnderWeight")
-elif bmi < 25:
-    print("Your BMI is {bmi}.You are UnderWeight")
-elif bmi < 25:
-    print("Your BMI is {bmi}.You are UnderWeight")
+    print(f"Your BMI is {bmi}.You have a Normal Weight")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}.You are OverWeight")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}.You are Obese")
+else:
+    print(f"Your BMI is {bmi}.You are Clinicaly obese")
